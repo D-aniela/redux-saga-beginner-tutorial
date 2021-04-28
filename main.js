33cc2ac3182940a4e8925d3e10a16873d64ed0c3
DanielEstr <laura.estrada@softtek.com>
2021-04-28 16:44:51 -0500

    Adding some tests to saga

--- main.js
+++ main.js
@@ -3,16 +3,17 @@ import createSagaMiddleware from 'redux-saga'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
-import { helloSaga } from './sagas'
+import rootSaga from './sagas'
 
 import Counter from './Counter'
 import reducer from './reducers'
 
+// call saga.js
 const sagaMiddleware = createSagaMiddleware()
 
 const store = createStore(reducer, applyMiddleware(sagaMiddleware))
 
-sagaMiddleware.run(helloSaga)
+sagaMiddleware.run(rootSaga)
 
 const action = (type) => store.dispatch({ type })
 
@@ -22,6 +23,8 @@ function render() {
       value={store.getState()}
       onIncrement={() => action('INCREMENT')}
       onDecrement={() => action('DECREMENT')}
+      // Connect onIncrementAsync of the component to a store action
+      onIncrementAsync={() => action('INCREMENT_ASYNC')}
     />,
     document.getElementById('root')
   )
--- sagas.js
+++ sagas.js
@@ -1,3 +1,27 @@
-export function* helloSaga() {
+import { all, call, put, takeEvery } from 'redux-saga/effects'
+
+export const delay = (ms) => new Promise((res) => setTimeout(res, ms))
+
+// export function* helloSaga() {
+function* helloSaga() {
   console.log('Hello Sagas!')
+}
+
+// worker saga: perform the async increment task
+export function* incrementAsync() {
+  // yield delay(1000)
+  // use the call Effect to call indirectly for test
+  yield call(delay, 1000) //=> {CALL: {fn: delay, args: [1000]}}
+  yield put({ type: 'INCREMENT' }) //=> {PUT: {type: 'INCREMENT'}}
+}
+
+// watcher saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
+// export function* watchIncrementAsync() {
+function* watchIncrementAsync() {
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
+
+// export functions in rootSaga func
+export default function* rootSaga() {
+  yield all([helloSaga(), watchIncrementAsync()])
 }
